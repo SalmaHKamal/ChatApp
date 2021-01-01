69a21819494d4b513b814f8c50b3cb5e1fdd8c1f
Salma Hassan <salmahassan087@gmail.com>
2021-01-01 19:39:24 +0200

    Release Notes :
    1] create and present LoginViewController

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -15,10 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		launchFirstScreen()
+		
 		return true
 	}
 
 
 
+	// MARK: - Helper Methods
+	private func launchFirstScreen() {
+		let viewController = LoginViewController()
+		window = UIWindow()
+		window?.rootViewController = viewController
+		window?.makeKeyAndVisible()
+	}
 }
 
---
+++ ChatApp/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by Salma Hassan on 1/1/21.
+//  Copyright © 2021 salma. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
