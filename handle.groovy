e2e13e36e227ae2d8360433bca14c3e7a465de29
Salma Hassan <salmahassan087@gmail.com>
2021-07-16 14:31:55 +0200

    Update handle.groovy

--- handle.groovy
+++ handle.groovy
@@ -15,6 +15,7 @@ Jenkins.instance.getItem("Test2")
 println("Hi agaain4")
 println("Hi agaain5")
 println("Hi agaain6")
+println("Hi agaain8")
 
 // def q = Jenkins.instance.queue
 // q.items.findAll { it.task.name.startsWith('Test') }.each { println(q)}
