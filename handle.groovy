94e5cda7cb216aef06168fa594d2d62e4d9c9733
SalmaHKamal <salmahassan087@gmail.com>
2021-07-16 15:41:57 +0200

    test groovy script

--- handle.groovy
+++ handle.groovy
@@ -8,7 +8,7 @@ import hudson.model.*
 // 	static void main(args) {
 
     println("Hellllooooo salma")
-    println(Jenkins.instance.queue.items)
+    Jenkins.instance.queue.items.each { println(it.task.name)}
 
 // def q = Jenkins.instance.queue
 // q.items.findAll { it.task.name.startsWith('Test') }.each { println(q)}
