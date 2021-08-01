8ca2d5145b666ce3d63cdae6fd2f50d2a9e51847
Salma Hassan <salmahassan087@gmail.com>
2021-08-01 21:06:00 +0200

    Update handle.groovy

--- handle.groovy
+++ handle.groovy
@@ -26,15 +26,27 @@ import hudson.model.*
 // // // }
 
 
+println 'print items ============='
 Jenkins.instance.queue.items.each { println(it.task.name)}
 def q = Jenkins.instance.queue
 //Find items in queue that match <project name>
-def queue = q.items.findAll { it.task.name.startsWith('Test') }
+def queue = q.items//.findAll { it.task.name.startsWith('Test') }
 //get all jobs id to list
 def queue_list = []
-queue.each { queue_list.add(it.getId()) }
+queue.each { 
+ println 'print ids ======'
+ println(it.getId())
+ queue_list.add(it.getId())
+}
+def sorted = queue_list.sort()
+println 'queue list ============'
+println(sorted)
 //sort id's, remove last one - in order to keep the newest job, cancel the rest
-queue_list.sort().take(queue_list.size() - 1).each { q.doCancelItem(it) }
+println 'last task =========='
+sorted.take(queue_list.size() - 1).each { 
+ println(it)
+ // q.doCancelItem(it) 
+}
 
 //piplineJob1, Test2, TestThree, Test4, TestPR
 // TestThree, Test4, TestPR, TestFirst, piplineJob1
