cac5a160c35e32c1b6f67630c821525680253bf1
Salma Hassan <salmahassan087@gmail.com>
2021-08-07 20:48:22 +0200

    Update handle.groovy

--- handle.groovy
+++ handle.groovy
@@ -25,8 +25,8 @@ import hudson.model.*
 // // // 	}
 // // // }
 
-println("CURRENT BUILD ==> " + currentBuild + " , number : ==> " + currentBuild.getNumber() + " AND Duration ===> " + currentBuild.getDuration() + "UPSTREAM Builds ==> " + currentBuild.getUpstreamBuilds())
-println("PR Target branch" + env.CHANGE_TARGET)
+println("CURRENT BUILD ==> " + currentBuild + " , number : ==> " + currentBuild.getNumber() + " AND Duration ===> " + currentBuild.getDuration() + " UPSTREAM Builds ==> " + currentBuild.getUpstreamBuilds())
+println("PR Target branch ==> " + env.CHANGE_TARGET)
 
 currentBuild.result = 'ABORTED'
 error('Stopping early…')
